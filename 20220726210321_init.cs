275c0211152e8406a27235bb1e50a71f1fa2979e
Bharath Kumar J <105350474+kumarbharath22@users.noreply.github.com>
2022-07-27 03:11:48 +0530

    Add files via upload
    AuditBenchmarkModule

---
+++ 20220726210321_init.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuditBenchmarkModule.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditBenchmarkList",
+                columns: table => new
+                {
+                    auditType = table.Column<string>(nullable: false),
+                    benchmarkNoAnswers = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditBenchmarkList", x => x.auditType);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditBenchmarkList");
+        }
+    }
+}
